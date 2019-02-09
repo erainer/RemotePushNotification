77bf09337b63446d665e2cc35cbe0e28885e5f33
Emily Rainer <emily.rainer@lextech.com>
2019-02-09 16:18:48 -0600

    Initial Commit

---
+++ RemotePushNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemotePushNotifications
+//
+//  Created by Emily Rainer on 2/9/19.
+//  Copyright © 2019 Emily Rainer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RemotePushNotificationsTests/RemotePushNotificationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  RemotePushNotificationsTests.swift
+//  RemotePushNotificationsTests
+//
+//  Created by Emily Rainer on 2/9/19.
+//  Copyright © 2019 Emily Rainer. All rights reserved.
+//
+
+import XCTest
+@testable import RemotePushNotifications
+
+class RemotePushNotificationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
